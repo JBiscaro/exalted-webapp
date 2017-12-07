65be3ee26302389c855f8d50788fcb62e888e581
Joseph Biscaro <j.biscaro@outlook.com>
2017-12-07 09:50:26 -0500

    added line break functionality to basic dynamic variable

--- calculator/views.py
+++ calculator/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # use something like commented code to put variables on webpage
-    # number = 6
+    number = 'Whatup?\nThis is on a new line?'
 
-    # return render(request, 'index.html', {'number': number})
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'number': number})
+    # return render(request, 'index.html')
 
 #   <p>{{ number }}</p>
 # add something like the preceding code in appropriate html file
