b51ce1d15c2f398f1191f667135bf2d46e78f992
Joseph Biscaro <j.biscaro@outlook.com>
2017-12-07 09:57:07 -0500

    added TODO

--- calculator/views.py
+++ calculator/views.py
@@ -11,3 +11,6 @@ def index(request):
 
 #   <p>{{ number }}</p>
 # add something like the preceding code in appropriate html file
+
+# TODO: find a way to dynamically add text to page, with line breaks,
+# as more options are selected
