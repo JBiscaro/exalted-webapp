04d7c74f074a7bdbeac6eb12690b7fd753b61f08
Joseph Biscaro <j.biscaro@outlook.com>
2017-12-07 16:55:47 -0500

    created database; added Charm model

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from calculator.models import Charm
+
+
 # Register your models here.
+class CharmAdmin(admin.ModelAdmin):
+    model = Charm
+    list_display = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Charm, CharmAdmin)
+
+# name: joe
+# pass: PredCyrus101@
